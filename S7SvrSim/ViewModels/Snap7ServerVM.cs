df6d21126e448adccac0c0047f44a7fd77b2c037
newbienewbie <itminus@hotmail.com>
2020-12-31 16:34:57 +0800

    界面：启动服务后，禁用“启动按钮”，禁用DB编辑功能

--- S7SvrSim/ViewModels/Snap7ServerVM.cs
+++ S7SvrSim/ViewModels/Snap7ServerVM.cs
@@ -85,6 +85,23 @@ namespace S7Svr.Simulator.ViewModels
             }
         }
 
+        #region 运行状态
+        private bool _runningStatus = false;
+        public bool RunningStatus
+        {
+            get => _runningStatus;
+            set
+            {
+                if (this._runningStatus != value)
+                {
+                    this._runningStatus = value;
+                    this.OnPropertyChanged(nameof(RunningStatus));
+                }
+            }
+        }
+        #endregion
+
+
         /// <summary>
         /// 正在运行的
         /// </summary>
