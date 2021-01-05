23418c2936ab68e98cd7ced53f1cc1ede4383796
newbienewbie <itminus@hotmail.com>
2021-01-05 11:22:33 +0800

    add sample scripts

---
+++ docs/sample scripts/UpdateServer.py
@@ -0,0 +1,4 @@
+s7_server_svc.WriteString(6, 2780, "1908WC16V299F6+YSTC1100139+L2/L3:1757;L1/N:1762;")
+s7_server_svc.WriteReal(6, 3036, 3545.2)
+s7_server_svc.WriteReal(6, 3040, 68.3)
+s7_server_svc.WriteReal(6, 3040, 4.8)
