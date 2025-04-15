72a89ae0fa8331bec1ac47639323e31d14ab3ea5
studylessshape <a664554724@qq.com>
2025-04-15 13:30:31 +0800

    feature:  DB 设置可以保存和加载

---
+++ src/S7SvrSim/Shared/StreamExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Text;
+
+namespace S7SvrSim.Shared
+{
+    internal static class StreamExtensions
+    {
+        public static void WriteString(this Stream stream, string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                return;
+            }
+
+            stream.Write(Encoding.UTF8.GetBytes(value));
+        }
+    }
+}
--- src/S7SvrSim/ViewModels/ServerVM/ConfigSnap7ServerVM.cs
+++ src/S7SvrSim/ViewModels/ServerVM/ConfigSnap7ServerVM.cs
@@ -1,7 +1,13 @@
-﻿using MediatR;
+﻿using FutureTech.Snap7;
+using MediatR;
 using ReactiveUI.Fody.Helpers;
 using S7Svr.Simulator.Messages;
+using S7SvrSim.Shared;
+using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Text;
@@ -15,6 +21,7 @@ namespace S7Svr.Simulator.ViewModels
     /// </summary>
     public class ConfigSnap7ServerVM : ReactiveObject
     {
+        private const string SERVER_ITEMS_SAVED_FILE = "areaconfig.csv";
         /// <summary>
         /// IP Address
         /// </summary>
@@ -25,7 +32,98 @@ namespace S7Svr.Simulator.ViewModels
         /// DB Configs
         /// </summary>
         public virtual ObservableCollection<AreaConfigVM> AreaConfigs { get; } = new ObservableCollection<AreaConfigVM>();
+
+        private string SavedFileName
+        {
+            get
+            {
+                var processPath = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+                if (processPath != null)
+                {
+                    return Path.Combine(processPath, SERVER_ITEMS_SAVED_FILE);
+                }
+                else
+                {
+                    return SERVER_ITEMS_SAVED_FILE;
+                }
+            }
+        }
+
+        public ConfigSnap7ServerVM()
+        {
+            AreaConfigs.CollectionChanged += AreaConfigs_CollectionChanged;
+            LoadAreaConfig();
+        }
+
+        private void AreaConfigs_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            SaveAreaConfig();
+            if (e.Action != System.Collections.Specialized.NotifyCollectionChangedAction.Move)
+            {
+                if (e.NewItems != null)
+                {
+                    foreach (var item in e.NewItems.Cast<AreaConfigVM>())
+                    {
+                        item.PropertyChanged += AreaConfigVM_PropertyChanged;
+                    }
+                }
+                if (e.OldItems != null)
+                {
+                    foreach (var item in e.OldItems.Cast<AreaConfigVM>())
+                    {
+                        item.PropertyChanged -= AreaConfigVM_PropertyChanged;
+                    }
+                }
+            }
+        }
+
+        private void AreaConfigVM_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            SaveAreaConfig();
+        }
+
+        private void LoadAreaConfig(string path = null)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                path = SavedFileName;
+            }
+
+            if (File.Exists(path))
+            {
+                var fileContent = File.ReadAllLines(path);
+                var areaKindType = typeof(AreaKind);
+                foreach (var line in fileContent.Skip(1))
+                {
+                    var items = line.Split(',');
+                    if (items.Length < 3)
+                    {
+                        break;
+                    }
+
+                    AreaConfigs.Add(new AreaConfigVM()
+                    {
+                        AreaKind = (AreaKind)Enum.Parse(areaKindType, items[0]),
+                        DBNumber = int.Parse(items[1]),
+                        DBSize = int.Parse(items[2]),
+                    });
+                }
+            }
+        }
+
+        private void SaveAreaConfig(string path = null)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                path = SavedFileName;
+            }
+
+            using var fileStream = new FileStream(path, FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
+            fileStream.WriteString(string.Join(",", nameof(AreaConfigVM.AreaKind), nameof(AreaConfigVM.DBNumber), nameof(AreaConfigVM.DBSize)));
+            foreach (var item in AreaConfigs)
+            {
+                fileStream.WriteString($"{Environment.NewLine}{string.Join(",", item.AreaKind, item.DBNumber, item.DBSize)}");
+            }
+        }
     }
-
-
 }
