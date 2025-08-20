4c2ded2167b9df91dfb65e07e2215f7f07fc4f4f
studylessshape <studylessshape@outlook.com>
2025-08-20 16:36:54 +0800

    fix: 添加回车提交IP地址(#15)

--- src/S7SvrSim/Commands/AppCommands.cs
+++ src/S7SvrSim/Commands/AppCommands.cs
@@ -1,9 +1,19 @@
-﻿using System.Windows.Input;
+﻿using Microsoft.Xaml.Behaviors.Core;
+using System.Windows;
+using System.Windows.Input;
 
 namespace S7SvrSim.Commands
 {
     public static class AppCommands
     {
         public static RoutedUICommand OpenFolder { get; } = new RoutedUICommand("打开所在目录", "OpenFolder", typeof(AppCommands));
+        public static ActionCommand LostFocus { get; } = new ActionCommand(() =>
+        {
+            if (Keyboard.FocusedElement != null)
+            {
+                Keyboard.FocusedElement.RaiseEvent(new RoutedEventArgs(UIElement.LostFocusEvent));
+                Keyboard.ClearFocus();
+            }
+        });
     }
 }
